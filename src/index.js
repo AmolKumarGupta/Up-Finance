6f52d7308c7fc01f6ecb3fc2a7effc9cb9b7fde1
amol <amlo1232001@gmail.com>
2023-05-07 14:32:06 +0530

    refactor: show dashboard as homepage

--- src/index.js
+++ src/index.js
@@ -25,9 +25,9 @@ ReactDOM.render(
       {/* add routes without layouts */}
       <Route path="/landing" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
+      <Route path="/" component={Admin} />
       {/* add redirect for first page */}
-      <Redirect from="*" to="/" />
+      <Redirect from="*" to="/admin" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
