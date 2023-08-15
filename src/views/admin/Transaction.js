4f2f8dde794612f7cebd5fc9dba0754c391d4a9d
amol <amlo1232001@gmail.com>
2023-08-16 01:31:26 +0530

    feat: pagination ui

--- src/views/admin/Transaction.js
+++ src/views/admin/Transaction.js
@@ -14,6 +14,10 @@ export default function Transaction() {
     amount: 0
   });
   const [errorBag, setErrorBag] = useState({});
+  const [tableMeta, setTableMeta] = useState({
+    page: 1,
+    limit: 10
+  });
 
   const handleDelete = async (id, rows = null) => {
     if (! id || rows===null) {
@@ -111,10 +115,19 @@ export default function Transaction() {
     }
   }
 
+  function navigatePage(data = 1) {
+    setTableMeta(prev => {
+      return {
+        ...prev, 
+        page: (prev.page && prev.page+data > 0) ? prev.page+data: 1
+      }
+    })
+  }
+
   return (
     <>
       <div className="flex flex-wrap relative">
-        <div className="w-full mb-12 mx-4 rounded text-blueGray-700 bg-white">
+        <div className="w-full mb-2 mx-4 rounded text-blueGray-700 bg-white">
           <div className="flex justify-between items-center py-2 px-4">
             <h3 className="font-semibold text-xl">Transactions</h3>
             <div className="flex gap-2 items-center ">
@@ -150,6 +163,57 @@ export default function Transaction() {
 
             </tfoot>
           </table>
+        </div>
+
+        <div className="mx-4 ms-auto mb-12 inline-flex justify-center gap-1">
+          <button onClick={() => navigatePage(-1)} className="inline-flex h-8 w-8 items-center justify-center rounded border border-white bg-lightBlue-600 text-white rtl:rotate-180 cursor-pointer">
+            <span className="sr-only">Prev Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+
+          <div>
+            <label htmlFor="PaginationPage" className="sr-only">Page</label>
+
+            <input
+              type="number"
+              className="h-8 w-12 rounded border border-gray-100 bg-white p-0 text-center text-xs font-medium text-gray-900 [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+              min="1"
+              value={tableMeta.page}
+              onChange={
+                (ev) => { ev.preventDefault() }
+              }
+            />
+          </div>
+
+          <button 
+            onClick={() => navigatePage(1)}
+            className="inline-flex h-8 w-8 items-center justify-center rounded border border-white bg-lightBlue-600 text-white rtl:rotate-180 cursor-pointer"
+          >
+            <span className="sr-only">Next Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </div>
 
